82763f35a5a99e495aefb62ef0495a028015cefa
GustavoH <98228045+GustavoHaguiar@users.noreply.github.com>
2024-02-20 12:20:37 -0300

    added dual core processing

--- Server/Server.ino
+++ Server/Server.ino
@@ -1,35 +1,58 @@
 #include <WiFi.h>
-#include <WebServer.h>
-#include <SPIFFS.h>
- 
-const char* ssid = "Access Point";
-const char* password =  "";
- 
-WebServer server(80);
- 
-void setup(){
-  Serial.begin(115200);
- 
-  if(!SPIFFS.begin()){
-        Serial.println("An Error has occurred while mounting SPIFFS");
-        return;
-  }
- 
-  WiFi.begin(ssid, password);
- 
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid, password);
+#include <WiFiClient.h>
 
-  Serial.println("Ponto de acesso criado com sucesso!");
-  Serial.print("Endereço IP do ESP32: ");
-  Serial.println(WiFi.softAPIP());
- 
-  server.on("/index", HTTP_GET, [](){
-    //server.send(200, "/index.html", "text/html");
-    server.send(200, "text/html", "/index.html");
-  });
- 
-  server.begin();
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+const int led = 2;
+const int input = 3;
+
+
+void setup(){
+  Serial.begin(115200); 
+
+  pinMode(led, OUTPUT);
+  pinMode(input, INPUT);
+
+  xTaskCreatePinnedToCore(
+    Task1code,   /* Task function. */
+    "Task1",     /* name of task. */
+    10000,       /* Stack size of task */
+    NULL,        /* parameter of the task */
+    1,           /* priority of the task */
+    &Task1,      /* Task handle to keep track of created task */
+    0            /* pin task to core 0 */  
+  );                          
+
+  xTaskCreatePinnedToCore(
+    Task2code,   /* Task function. */
+    "Task2",     /* name of task. */
+    10000,       /* Stack size of task */
+    NULL,        /* parameter of the task */
+    1,           /* priority of the task */
+    &Task2,      /* Task handle to keep track of created task */
+    1            /* pin task to core 1 */
+  );          
 }
- 
+
+void Task1code( void * pvParameters ){
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  Connection();
+}
+
+//Task2code: blinks an LED every 700 ms
+void Task2code( void * pvParameters ){
+  Serial.print("Task2 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for(;;){
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(1000);
+  }
+}
+
 void loop(){}
